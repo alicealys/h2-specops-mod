1c25f7ef7d5d16e7a6ff400068576ec58e30c690
fed <58637860+fedddddd@users.noreply.github.com>
2022-09-26 22:17:05 +0200

    Remove slide on boneyard

--- specops/scripts/specops/maps/so_intel_boneyard.lua
+++ specops/scripts/specops/maps/so_intel_boneyard.lua
@@ -2,6 +2,7 @@ local map = {}
 
 map.premain = function()
     setloadout("m4m203_reflex", "cheytac", "fraggrenade", "flash_grenade", "viewhands_tf141", "american")
+    game:getentarray("trigger_multiple_slide", "classname"):foreach(entity.delete)
 end
 
 map.main = function()
